f0d503fe3a8052bcbbd7832d7e1aa8fd4762d206
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-18 07:39:00 -0600

    Bienes raíces, NodeJS: Implementando Controllers

---
+++ controllers/usuarioController.js
@@ -0,0 +1,10 @@
+
+const formularioLogin = (req, res) => {
+    res.render('auth/login', {
+        autenticado: false
+    })
+}
+
+export {
+    formularioLogin
+}
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,11 +1,8 @@
 import express from "express";
+import {formularioLogin} from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-    res.render('auth/login', {
-        autenticado: false
-    })
-});
+router.get("/login", formularioLogin);
 
 export default router;
