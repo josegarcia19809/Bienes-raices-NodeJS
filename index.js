c42adf8614539d4be5cf5005d1ac78b8b8a35320
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-19 09:30:21 -0600

    Bienes raíces, NodeJS: Conectando nuestra app a una base de datos

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 // Crear la app
 const app = express();
+
+// Conexión a la base datos
+try{
+    await db.authenticate();
+    console.log("Conexión correcta a la base de datos");
+
+}catch (e) {
+    console.log(e);
+}
 
 // Habilitar PUG
 app.set("view engine", "pug");
