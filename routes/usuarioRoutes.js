b1660cf539d31a65fa50c875011c686a2d239db6
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-29 19:59:03 -0600

    Bienes raíces, NodeJS: Comunicando la Vista con el routing y el Controller

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
     formularioLogin,
     formularioOlvidePassword,
-    formularioRegistro
+    formularioRegistro,
+    registrar
 } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.post("/registro", registrar);
 router.get("/olvide-password", formularioOlvidePassword);
 
 export default router;
