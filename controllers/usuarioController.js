eceeacc5f4302b856fb89e04035303a5e67d61fa
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-30 08:12:08 -0600

    Bienes raíces, NodeJS: Auto llenando los campos que pasaron la validación

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -20,12 +20,13 @@ const formularioOlvidePassword = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    console.log(req.body);
     // Validación
     await check("nombre").notEmpty().withMessage("El nombre es obligatorio").run(req);
     await check("email").isEmail().withMessage("El email no es válido").run(req);
     await check("password").isLength({min: 6}).withMessage("El password debe tener 6" +
         " caracteres").run(req);
-    await check("repetir_password").equals("password").withMessage("Los passwords no son" +
+    await check("repetir_password").equals(req.body.password).withMessage("Los passwords no son" +
         " iguales").run(req);
 
     let resultado = validationResult(req);
@@ -35,7 +36,11 @@ const registrar = async (req, res) => {
         // Hubo errores
         return res.render('auth/registro', {
             pagina: "Crear cuenta",
-            errores: resultado.array()
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
         });
     }
 
