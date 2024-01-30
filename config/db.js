af4466b3708d408959359fe3fbd4bd8a99f71430
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-29 19:42:22 -0600

    Bienes raíces, NodeJS: Ocultando la información de la base de datos en variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,19 +1,23 @@
 import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize("bienesraices_node_mvc", "root", "", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    define: {
-        timestamps: true
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    // operatorsAliases: false
-});
+dotenv.config({path: '.env'});
+
+const db = new Sequelize(process.env.BD_NOMBRE,
+    process.env.BD_USER, process.env.BD_PASS ?? '', {
+        host: process.env.BD_HOST,
+        port: 3306,
+        dialect: "mysql",
+        define: {
+            timestamps: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        // operatorsAliases: false
+    });
 
 export default db;
