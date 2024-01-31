c52acbf123a435b0643fe21c8fcf5a81368f68b2
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-30 20:30:29 -0600

    Bienes raíces, NodeJS: Confirmar si el Token es Válido o no

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -83,10 +83,18 @@ const registrar = async (req, res) => {
     })
 }
 
-const confirmar = (req, res) => {
+const confirmar = async (req, res) => {
     const {token} = req.params;
 
     // Verificar si el token es válido
+    const usuario = await Usuario.findOne({where: {token}});
+    if (!usuario) {
+        return res.render("auth/confirmar-cuenta", {
+            pagina: "Error al confirmar tu cuenta",
+            mensaje: "Hubo un error al confirmar tu cuenta, intenta de nuevo",
+            error: true
+        });
+    }
 
     // Confirmar la cuenta
 }
