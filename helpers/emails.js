6968d3ac4ab5678c48f5e3bd56106ac5726e7d3e
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-30 18:34:50 -0600

    Bienes raíces, NodeJS: Enviando Emails con NodeMailer y Mailtrap

---
+++ helpers/emails.js
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer";
+
+const emailRegistro = async (datos) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
+
+export {
+    emailRegistro
+}
