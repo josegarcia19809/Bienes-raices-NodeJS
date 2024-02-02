3da745a9baf6a9e28441875830e122ee35ca5496
josegarcia19809 <josegarcia19809@gmail.com>
2024-02-02 12:21:31 -0600

    Bienes raíces, NodeJS: Comenzando el Formulario de Crear Propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,9 +1,19 @@
 const admin = (req, res) => {
     res.render("propiedades/admin",{
-        pagina: "Mis propiedades"
+        pagina: "Mis propiedades",
+        barra: true
+    })
+}
+
+// Formulario para crear una nueva propiedad
+const crear = (req, res) => {
+    res.render("propiedades/crear",{
+        pagina: "Crear propiedad",
+        barra: true
     })
 }
 
 export {
-    admin
+    admin,
+    crear
 }
