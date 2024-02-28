973af7d68261b054b635f3cf77e4f3719058e755
josegarcia19809 <josegarcia19809@gmail.com>
2024-02-27 21:20:34 -0600

    Bienes raíces, NodeJS: Añadiendo un Pin con la ubicación de la Propiedad

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,14 +1,21 @@
-(function() {
+(function () {
 
     // Logical Or
     const lat = 19.73146275420665;
     const lng = -99.95778290450444;
-    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    const mapa = L.map('mapa').setView([lat, lng], 16);
+    let marker;
 
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+    // El pin
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    })
+        .addTo(mapa)
 
 
 })()
