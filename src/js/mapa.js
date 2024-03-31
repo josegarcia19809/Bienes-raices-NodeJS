de15824327db8f1b4d892ffbfb4f84793c170533
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-30 21:16:02 -0600

    Bienes raíces, NodeJS: Obtener Información de la Calle y Ciudad al soltar el Pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -6,6 +6,9 @@
     const mapa = L.map('mapa').setView([lat, lng], 16);
     let marker;
 
+    // Utilizar Provider y Geocoder
+    const geocodeService= L.esri.Geocoding.geocodeService();
+
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -22,6 +25,11 @@
         marker = e.target;
         const posicion = marker.getLatLng();
         mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
+
+        // Obtener la información de las calles al soltar el pin
+        geocodeService.reverse().latlng(posicion, 13).run(function (error, resultado) {
+            marker.bindPopup(resultado.address.LongLabel);
+        })
     })
 
 })()
