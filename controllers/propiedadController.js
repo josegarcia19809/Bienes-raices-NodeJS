e402e9646af14af7ba8c04c0d7500ddf46e2289f
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-31 19:28:32 -0600

    Bienes raíces, NodeJS: Importando Precios y Categorias para Pasarlos a la Vista de Crear

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,15 +1,26 @@
+import Precio from "../models/Precio.js";
+import Categoria from "../models/Categoria.js";
+
 const admin = (req, res) => {
-    res.render("propiedades/admin",{
+    res.render("propiedades/admin", {
         pagina: "Mis propiedades",
         barra: true
     })
 }
 
 // Formulario para crear una nueva propiedad
-const crear = (req, res) => {
-    res.render("propiedades/crear",{
+const crear = async (req, res) => {
+    // Consultar Modelos de Precio y Categoria
+    const [categorias, precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+    ]);
+
+    res.render("propiedades/crear", {
         pagina: "Crear propiedad",
-        barra: true
+        barra: true,
+        categorias,
+        precios
     })
 }
 
