d8dd8cf7311791510b5dee3f3dfa1647328ca3e6
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-31 21:10:37 -0600

    Bienes raíces, NodeJS: Añadiendo CSRF y Validación al Formulario

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,5 +1,6 @@
 import Precio from "../models/Precio.js";
 import Categoria from "../models/Categoria.js";
+import {validationResult} from "express-validator";
 
 const admin = (req, res) => {
     res.render("propiedades/admin", {
@@ -19,12 +20,35 @@ const crear = async (req, res) => {
     res.render("propiedades/crear", {
         pagina: "Crear propiedad",
         barra: true,
+        csrfToken: req.csrfToken(),
         categorias,
         precios
     })
 }
 
+const guardar = async (req, res) => {
+    // Validación
+    let resultado = validationResult(req);
+    if (!resultado.isEmpty()) {
+        // Consultar Modelos de Precio y Categoria
+        const [categorias, precios] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+        ]);
+
+        res.render("propiedades/crear", {
+            pagina: "Crear propiedad",
+            barra: true,
+            csrfToken: req.csrfToken(),
+            categorias,
+            precios,
+            errores: resultado.array(),
+        })
+    }
+}
+
 export {
     admin,
-    crear
+    crear,
+    guardar
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
+import {body} from "express-validator";
 import {
     admin,
-    crear
+    crear,
+    guardar
 } from "../controllers/propiedadController.js";
 
 const router = express.Router();
 
 router.get("/mis-propiedades", admin);
 router.get("/propiedades/crear", crear);
+router.post("/propiedades/crear",
+    body("titulo").notEmpty().withMessage("El titulo del anuncio es obligatorio"),
+    guardar);
 
 export default router;
