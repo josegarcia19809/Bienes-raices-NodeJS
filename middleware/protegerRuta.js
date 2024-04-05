23a73beb897ca7a70189d451a117fa6b9f7585cb
josegarcia19809 <josegarcia19809@gmail.com>
2024-04-04 20:24:34 -0600

    Bienes raíces, NodeJS: Leyendo y Validando el JWT

--- middleware/protegerRuta.js
+++ middleware/protegerRuta.js
@@ -1,9 +1,21 @@
+import jwt from "jsonwebtoken";
+import {Usuario} from "../models/index.js";
+
 const protegerRuta = async (req, res, next) => {
     console.log("Desde el middleware");
 
     // Verificar si hay un token
-
+    const {_token} = req.cookies;
+    if (!_token) {
+        return res.redirect("/auth/login");
+    }
     // Comprobar el Token
+    try{
+        const decoded=jwt.verify(_token, process.env.JWT_SECRET);
+        const usuario = await Usuario.findByPk(decoded.id);
+    }catch(err){
+        return res.clearCookie('_token').redirect("auth/login");
+    }
 
     next();
 }
